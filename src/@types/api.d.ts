9600518a88f9d1db6b8e6812c352419c7245f888
SAudrius <audrius@sabalys.com>
2024-04-03 23:38:45 +0300

    feat(type): new type for Ads

--- src/@types/api.d.ts
+++ src/@types/api.d.ts
@@ -7,3 +7,23 @@ interface User {
   avatar_url: string;
   is_deleted: string;
 }
+interface Ads {
+  id: number;
+  title: string;
+  main_image_url: string;
+  description: string;
+  price: number;
+  phone: string;
+  type: Type;
+  town_id: number;
+  user_id: number;
+  category_id: number;
+  created_at: string;
+  is_published: boolean;
+}
+
+enum Type {
+  sell,
+  buy,
+  rent,
+}
