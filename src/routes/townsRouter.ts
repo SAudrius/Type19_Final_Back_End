9d880482027261a1d9e8dd84d89cfae571cdae0c
SAudrius <audrius@sabalys.com>
2024-04-04 12:35:57 +0300

    build(route): create towns router

---
+++ src/routes/townsRouter.ts
@@ -0,0 +1,57 @@
+import express from 'express';
+import { ResultSetHeader } from 'mysql2';
+
+import { dbQuery, sendJsonError } from '../helpers/index.js';
+
+const townsRouter = express.Router();
+
+townsRouter.get('/', async (req, res) => {
+  const sql = 'SELECT * FROM towns WHERE is_deleted = 0';
+  const [rows, error] = await dbQuery<Town[]>(sql);
+  if (error) {
+    sendJsonError(res);
+    return;
+  }
+  res.json(rows);
+});
+
+townsRouter.get('/:id', async (req, res) => {
+  const townId = req.params.id;
+  const sql = 'SELECT * FROM towns WHERE is_deleted = 0 AND id = ?';
+  const dbParams = [townId];
+  const [rows, error] = await dbQuery<Town[]>(sql, dbParams);
+  if (error) {
+    sendJsonError(res);
+    return;
+  }
+  res.json(rows);
+});
+
+townsRouter.post('/', async (req, res) => {
+  const { name, area, population } = req.body as Town;
+  const dbParams = [name, area, population];
+  const sql = 'INSERT INTO towns (name, population, area) VALUES (?,?,?)';
+  const [rows, error] = await dbQuery<ResultSetHeader>(sql, dbParams);
+  if (error) {
+    sendJsonError(res);
+    return;
+  }
+  res.json({ id: rows.insertId, ...req.body });
+});
+
+townsRouter.delete('/:id', async (req, res) => {
+  const townId = req.params.id;
+  const sql = 'UPDATE towns SET is_deleted = 1 WHERE id = ?';
+  const dbParams = [townId];
+  const [rows, error] = await dbQuery<ResultSetHeader>(sql, dbParams);
+  if (error) {
+    sendJsonError(res);
+    return;
+  }
+  if (rows.affectedRows < 1) {
+    sendJsonError(res, 404, { message: 'town not found' });
+  }
+  res.sendStatus(200);
+});
+
+export default townsRouter;
